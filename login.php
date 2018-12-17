5f742e01a2b732ed559fbcedc850ef61582a82f5
Jojie Jagonos <jojiejagonos@elink.corp>
2018-12-18 07:35:38 +0900

    add myStyle.css

--- login.php
+++ login.php
@@ -94,12 +94,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+    <link rel = "stylesheet" type = "text/css" href = "myStyle.css" />
 </head>
 <body>
+    <h1 class="title">ReadersMagnet Author Lounge </h1>
     <div class="wrapper">
         <h2>Login</h2>
         <p>Please fill in your credentials to login.</p>
--- register.php
+++ register.php
@@ -108,12 +108,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <title>Sign Up</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+    <link rel = "stylesheet" type = "text/css" href = "myStyle.css" />
 </head>
 <body>
+    <h1 class="title">ReadersMagnet Author Lounge </h1>
     <div class="wrapper">
         <h2>Sign Up</h2>
         <p>Please fill this form to create an account.</p>
